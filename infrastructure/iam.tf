0f6dc456537214da91dd7ae3575fb2cc4c5427b3
Berret Terry <berretterry@gmail.com>
2025-06-13 13:04:43 -0700

    made some iam permission changes based on aws logs about the failure

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -37,10 +37,17 @@ resource "aws_iam_policy" "this_task_execution" {
       },
 
       {
-        Action = "ssm:GetParameter*"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "ssm:GetParameter*",
+          "kms:Decrypt"
+        ]
         Effect = "Allow"
         Resource = [
-          aws_ssm_parameter.secret_key.arn,
+          "${aws_ssm_parameter.secret_key.arn}",
+          "arn:aws:ssm:us-west-2:637423342822:parameter/*",
+          "arn:aws:secretsmanager:us-west-2:637423342822:secret:*",
+          "arn:aws:kms:us-west-2:637423342822:key/*"
         ],
       },
     ]
--- infrastructure/ssm.tf
+++ infrastructure/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "secret_key" {
-  name = "/strongdm/proxy-cluster/${var.name}/secret-key"
+  name = "/strongdm/proxy-cluster/${var.name}-proxy/secret-key"
   type = "SecureString"
 
   value = var.sdm_proxy_cluster_secret_key
